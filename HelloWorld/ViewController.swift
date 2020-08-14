edacec625f8be2b46a8c357ce06e7bba9c54fa23
Shin <shintaroaraya0801@icloud.com>
2020-08-14 11:50:11 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 荒屋伸太朗 on 2020/08/14.
+//  Copyright © 2020 shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
